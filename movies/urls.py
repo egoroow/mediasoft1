3ee3f87bc221a6ac5c6640e22f2affcbcd1caa5e
egoroow <87124104+egoroow@users.noreply.github.com>
2021-08-27 20:03:12 +0400

    Update urls

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .models import Movie, Actor, Genre, Producer
+from .forms import MovieForm, ActorForm, ProducerForm, GenreForm
 from django.contrib.auth.views import LogoutView
 
 app_name = 'movies'
@@ -9,18 +11,12 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('registration/', views.RegistrationView.as_view(), name='registration'),
-    path('add/movie', views.MovieCreateView.as_view(), name='add_movie'),
-    path('movie/<slug:pk>/remove/', views.MovieDeleteView.as_view(), name='delete_movie'),
-    path('movie/<slug:pk>/edit/', views.MovieUpdateView.as_view(), name='update_movie'),
-    path('add/actor', views.ActorCreateView.as_view(), name='add_actor'),
-    path('actor/<slug:pk>/remove/', views.ActorDeleteView.as_view(), name='delete_actor'),
-    path('actor/<slug:pk>/edit/', views.ActorUpdateView.as_view(), name='update_actor'),
-    path('add/genre', views.GenreCreateView.as_view(), name='add_genre'),
-    path('genre/<slug:pk>/remove/', views.GenreDeleteView.as_view(), name='delete_genre'),
-    path('genre/<slug:pk>/edit/', views.GenreUpdateView.as_view(), name='update_genre'),
-    path('add/producer', views.ProducerCreateView.as_view(), name='add_producer'),
-    path('producer/<slug:pk>/remove/', views.ProducerDeleteView.as_view(), name='delete_producer'),
-    path('producer/<slug:pk>/edit/', views.ProducerUpdateView.as_view(), name='update_producer'),
+    path('add/movie', views.ModelCreateView.as_view(model=Movie, form_class=MovieForm), name='add_movie'),
+    path('movie/<slug:pk>/remove/', views.ModelDeleteView.as_view(model=Movie, permission_required='movies.delete_movie'), name='delete_movie'),
+    path('movie/<slug:pk>/edit/', views.ModelUpdateView.as_view(model=Movie, form_class=MovieForm, permission_required='movies.change_movie'), name='update_movie'),
+    path('add/actor', views.ModelCreateView.as_view(model=Actor, form_class=ActorForm), name='add_actor'),
+    path('add/genre', views.ModelCreateView.as_view(model=Genre, form_class=GenreForm), name='add_genre'),
+    path('add/producer', views.ModelCreateView.as_view(model=Producer, form_class=ProducerForm), name='add_producer'),
     path('search/genre/<int:genre_id>', views.search, name='search_genre'),
     path('search/actor/<int:actor_id>', views.search, name='search_actor'),
     path('search/producer/<int:producer_id>', views.search, name='search_producer'),
